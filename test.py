00abec6f8d609cf876d9e4e1984a08bc6818decb
AaryanPalve5 <aspalve2004@gmail.com>
2025-05-30 17:46:40 +0530

    Post augmentation notebook uploading

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ import cv2
 model = YOLO("vision2.pt")
 
 # Path to the image you want to detect
-image_path = "Raw dataset/14.png"  # CHANGE THIS to your image
+image_path = "Raw dataset/16.png"  # CHANGE THIS to your image
 
 # Run detection
 results = model(image_path, save=True, conf=0.4)  # save=True will save results in runs/detect
