abfd1df55e75c1751e7ed5d34557667881a7dd69
AaryanPalve5 <aspalve2004@gmail.com>
2025-05-30 17:40:54 +0530

    Initial commit (model + evidencee)

---
+++ Doors and Windows/README.dataset.txt
@@ -0,0 +1,6 @@
+# Doors and Windows > 2025-05-30 3:05pm
+https://universe.roboflow.com/asp-2t4fy/doors-and-windows-cgn3z
+
+Provided by a Roboflow user
+License: CC BY 4.0
+
---
+++ test.py
@@ -0,0 +1,21 @@
+# detect_image.py
+
+from ultralytics import YOLO
+import cv2
+
+# Load the trained model
+model = YOLO("vision2.pt")
+
+# Path to the image you want to detect
+image_path = "Raw dataset/14.png"  # CHANGE THIS to your image
+
+# Run detection
+results = model(image_path, save=True, conf=0.4)  # save=True will save results in runs/detect
+
+# Optional: Display the image with detections
+result_image = results[0].plot()
+cv2.imshow("Detection", result_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+print("Detection complete.")
